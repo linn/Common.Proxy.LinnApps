5da300054afac9bc9513995a04b2f52079f180da
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-04-25 15:47:44 +0100

    implementation

--- src/Services/BartenderLabelPack.cs
+++ src/Services/BartenderLabelPack.cs
@@ -9,9 +9,68 @@
 
     public class BartenderLabelPack : ILabelPrinter
     {
-        public Task<(bool Success, string Message)> PrintLabelsAsync(string fileName, string printer, int qty, string template, string data)
+        public async Task<(bool Success, string Message)> PrintLabelsAsync(string fileName, string printer, int qty, string template, string data)
         {
-            throw new System.NotImplementedException();
+            var connection = new OracleConnection(ConnectionStrings.ManagedConnectionString());
+            var cmd = new OracleCommand("BARTENDER.PRINT_LABELS_WRAPPER", connection)
+                          {
+                              CommandType = CommandType.StoredProcedure
+                          };
+
+            var result = new OracleParameter(null, OracleDbType.Int32)
+                             {
+                                 Direction = ParameterDirection.ReturnValue,
+                                 Size = 2000
+                             };
+            cmd.Parameters.Add(result);
+
+            cmd.Parameters.Add(
+                new OracleParameter("p_filename", OracleDbType.Varchar2)
+                    {
+                        Direction = ParameterDirection.Input,
+                        Size = 50,
+                        Value = fileName
+                    });
+            cmd.Parameters.Add(
+                new OracleParameter("p_printer", OracleDbType.Varchar2)
+                    {
+                        Direction = ParameterDirection.Input,
+                        Size = 100,
+                        Value = printer
+                    });
+            cmd.Parameters.Add(
+                new OracleParameter("p_qty", OracleDbType.Int32) { Direction = ParameterDirection.Input, Value = qty });
+            cmd.Parameters.Add(
+                new OracleParameter("p_template", OracleDbType.Varchar2)
+                    {
+                        Direction = ParameterDirection.Input,
+                        Size = 100,
+                        Value = template
+                    });
+            cmd.Parameters.Add(
+                new OracleParameter("p_data", OracleDbType.Varchar2)
+                    {
+                        Direction = ParameterDirection.Input,
+                        Size = 4000,
+                        Value = data
+                    });
+            var message = string.Empty;
+
+            cmd.Parameters.Add(
+                new OracleParameter("p_message", OracleDbType.Varchar2)
+                    {
+                        Direction = ParameterDirection.InputOutput,
+                        Size = 500,
+                        Value = message
+                    });
+
+            await connection.OpenAsync();
+            await cmd.ExecuteNonQueryAsync();
+            await connection.CloseAsync();
+
+            var success = result.Value.ToString() == "1";
+
+            return (success, message);
         }
     }
 }
